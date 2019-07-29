b22ce4039fb4b59e2371b0fa50665aa60db43854
Hrealm <952172047@qq.com>
2019-07-29 16:04:00 +0800

    updated gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
+const gls = require('gulp-live-server');
+const server = gls.static('dist',8080);
+
+// gulp.task('template',()=>{
+
+// })
