4630b716e370a18a741e5f259d86f34af97b9dc3
Hrealm <952172047@qq.com>
2019-07-29 22:12:10 +0800

    updated container

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,45 @@
 const gulp = require('gulp');
 const fs = require('fs');
 const path = require('path');
+const ejs = require('gulp-ejs');
+const sass = require('gulp-sass');
 const gls = require('gulp-live-server');
-const server = gls.static('dist',8080);
+const server = gls.static('dist',8899);
 
-// gulp.task('template',()=>{
+const resumePath = path.join(__dirname,'resume.json');
+const templatePath = path.join(__dirname,'template');
+const stylesheetPath = path.join(__dirname,'stylesheet')
 
-// })
+function getData(resumePath){
+    const resume = JSON.parse(fs.readFileSync(resumePath));
+    const language = JSON.parse(fs.readFileSync(path.join(__dirname,'/language/'+ resume.language + '.json')));
+    let data = Object.assign(resume,language);
+    return data;
+}
+
+gulp.task('template',()=>{
+    const data = getData(resumePath);
+    return gulp.src(path.join(templatePath,'index.html'))
+        .pipe(ejs(data))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('stylesheet',()=>{
+    return gulp.src(path.join(stylesheetPath,'style.scss'))
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('watch',gulp.series('template','stylesheet',()=>{
+    server.start();
+
+    gulp.watch([
+        path.join(__dirname,'language','**'),
+        path.join(templatePath,'**','*'),
+        path.join(resumePath)
+    ],gulp.series('template'));
+
+    gulp.watch([path.join(stylesheetPath,'**','*')],gulp.series('stylesheet'));
+
+}));
